159bd79938f182d3daa539aef1389f461cca9cc2
PaquinWyattA <PaquinWyattA@WYATT-PC>
2016-10-07 23:13:27 -0400

    Begin work on Class methods

---
+++ LAB01 Calc/Calculator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB01_Calc
+{
+    public class Calculator
+    {
+        public decimal currentValue;
+        public decimal enteredValue;
+
+        //Constructor
+        public Calculator()
+        {
+            this.CurrentValue = currentValue;
+        }
+
+        //Property to keep track of the result currently displayed by the calculator
+        public decimal CurrentValue
+        {
+            get
+            {
+                return currentValue;
+            }
+            set
+            {
+                currentValue = value;
+            }
+        } 
+
+        //
+        public string EnterValue()
+        {
+
+        }
+
+
+
+
+
+
+
+    }
+}
